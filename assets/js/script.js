beffc4d1661e8464a2e90db01ab8528be60e1ff1
JoaoGuimaraes-11 <jonyjocas11@gmail.com>
2025-04-22 22:20:56 -0300

    adicionando funcionalidade de mostrar mais imagens

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const showPics = document.querySelector('#showPics');
+
+showPics.addEventListener('click', ()=>{
+    const showImages = document.querySelector('.gallery-container-hidden');
+    showImages.classList.toggle('active');
+    if(showImages.classList.contains('active')){
+        showPics.innerText = 'Ver menos'
+    }else{
+        showPics.innerText = 'Ver mais'
+    }
+})
